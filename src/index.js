5778695cd872c7e6c47dcd06c1cc528d78d69e52
Kozazavr <sohee@yandex.ru>
2021-02-05 22:58:38 +0300

    typical

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array == undefined || array == 0) {
+    return 0;
+  }
+  let arr = array.sort((a, b) => a - b);
+  return arr[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == undefined || array == 0) {
+    return 0;
+  }
+  let arr = array.sort((a, b) => a - b);
+  return arr[arr.length - 1];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == undefined || array == 0) {
+    return 0;
+  }
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum / array.length;
 }
